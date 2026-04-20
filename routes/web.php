376822419b81806711004040d1c0fd07842637eb
Nabilah <NABILAH@LAPTOP-4CCV5UVI>
2026-04-20 09:06:59 +0700

    edit by bila

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloWorldController;
 use App\Http\Controllers\AnggotaController;
-use App\Http\Controllers\LoginController; 
 use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DestinasiController;
